6427f43b85b982f037b19a8c90df92f782c0eeeb
Tarun Chopra <tarun.chopra10@gmail.com>
2022-01-16 09:00:51 +0530

    Two way data binding for search action in component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -9,13 +9,20 @@ import { ActivatedRoute } from '@angular/router';
 export class SearchComponent implements OnInit {
 
   searchTerm:String = "";
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      if(params['searchTerm'])
+      if(params['searchTerm']) {
         this.searchTerm = params['searchTerm'];
+      }
     })
+  }
+
+  search():void {
+    if(this.searchTerm) {
+      this.router.navigateByUrl('/search/' + this.searchTerm);
+    }
   }
 
 }
