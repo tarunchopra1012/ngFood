e6714ad2fc485327c667644ce48ab7b7a6ddfb1e
Tarun Chopra <tarun.chopra10@gmail.com>
2022-01-16 21:49:13 +0530

    Filter food by Tags functionality

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Food } from '../../shared/models/Food';
+import { Tag } from 'src/app/shared/models/Tag';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,23 @@ import { Food } from '../../shared/models/Food';
 export class FoodService {
 
   constructor() { }
+
+  getAllTags(): Tag[] {
+    return [
+      { name: 'All', count: 14 },
+      { name: 'FastFood', count: 4 },
+      { name: 'Pizza', count: 2 },
+      { name: 'Lunch', count: 3 },
+      { name: 'SlowFood', count: 2 },
+      { name: 'Hamburger', count: 1 },
+      { name: 'Fry', count: 1 },
+      { name: 'Soup', count: 1 },
+    ];
+  }
+
+  getAllFoodsByTag(tag:string): Food[] {
+    return tag == 'All' ? this.getAll() : this.getAll().filter(food => food.tags?.includes(tag))
+  }
 
   getAll(): Food[]{
     return [
@@ -77,5 +95,9 @@ export class FoodService {
         tags: ['FastFood', 'Pizza', 'Lunch'],
       },
     ];
+  }
+
+  getAllFoodsBySearchTerm(searchTerm: string): Food[] {
+    return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
   }
 }
